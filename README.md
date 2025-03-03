eb05ba85001332cdf63127664316bc3b4f7546fb
Alain Chan <cv112113@gmail.com>
2025-03-04 02:05:11 +0800

    Initialize Flutter project with basic structure and configurations

---
+++ README.md
@@ -0,0 +1,3 @@
+# voltcounter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/voltcounter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.voltcounter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
